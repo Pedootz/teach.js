57e37e824c315e57099445c6791fb84c583c3f61
Thomas Peduto <tpeduto@stargroup1.com>
2014-01-14 20:33:57 -0500

    lesson 1 partial commit

---
+++ mug_googler.js/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<title>Moogle!</title>
+<link rel="stylesheet" href="style.css" type="text/css" />
+<script src="scripts.js"></script>
+</head>
+<body>
+
+<h1>Welcome to Moogle!</h2>
+
+<img id="profileImg" src="/img/mystery.jpg"/>
+<h3 id="profileName">Pick a Mug!</h3>
+<a id="profileWiki">Learn more on Wikipedia!</a>
+
+<div id="controlWrap">
+	<form id="controlForm">
+
+		<select>
+			<option value="1">Bill</option>
+			<option value="2">Bob</option>
+			<option value="3">Beaker</option>
+			<option value="4">Barney</option>
+		</select>
+
+		<input type="button" value="Take Me Away!"/>
+
+	</form>
+</div>
+
+</body>
